d28d7e7d96764be45fdf3683f788785f3d426888
shyam-lal <slal4252@gmail.com>
2021-01-16 00:01:02 +0530

    First

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os 
 
-root = tk.Tk()
+root = tk.Tk(screenName="Sudoku")
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
 
 root.mainloop()
